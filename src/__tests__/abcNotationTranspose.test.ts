02eaea858eec125e7f546e8bfb4534eab4031fab
im-not-enemy <opkd0710@gmail.com>
2020-10-10 07:48:49 +0000

    npm公開用

--- src/__tests__/abcNotationTranspose.test.ts
+++ src/__tests__/abcNotationTranspose.test.ts
@@ -1,4 +1,4 @@
-import {abcNotationTranspose} from '../index'
+import abcNotationTranspose from '../index'
 
 test('C to G',()=>{
     const before = 'T:Test\nM:4/4\nL:1/4\nK:C\nC/D/E/F/ G/F/E/D/ | C/B,/A,/G,/ A,/B,/C ||'
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import Song from './Application/Song'
 
-export function abcNotationTranspose(abc:string,key:string):string {
+export default function abcNotationTranspose(abc:string,key:string):string {
     return new Song(abc).transpose(key).toString()
 }
